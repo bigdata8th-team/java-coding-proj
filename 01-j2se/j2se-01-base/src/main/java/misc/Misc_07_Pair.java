ca6d34f368256fb35cfc71a71279c3b4e1e9c052
gaosongling <mail.xiaomi.com>
2018-04-09 10:12:00 +0800

    修改了pair的代码

--- 01-j2se/j2se-01-base/src/main/java/misc/Misc_07_Pair.java
+++ 01-j2se/j2se-01-base/src/main/java/misc/Misc_07_Pair.java
@@ -3,17 +3,14 @@ package misc;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Misc_07_Pair {
     public static void main(String[] args) {
         Map<Pair<String, String>, Long> countMap = new HashMap<>();
         Pair<String, String> stu1 = new ImmutablePair<>("1", "hank");
         Pair<String, String> stu2 = new ImmutablePair<>("1", "bob");
-        Pair<String, String> stu3 = new ImmutablePair<>("1", "james");
+        Pair<String, String> stu3 = new ImmutablePair<>("2", "james");
         List<Pair<String, String>> countList = new ArrayList<>();
         countList.add(stu1);
         countList.add(stu2);
@@ -26,5 +23,18 @@ public class Misc_07_Pair {
                 .ifPresent(
                         System.out::println
                 );
+        Set<String> keySet = new HashSet<>();
+        countList.forEach(line -> keySet.add(line.getKey()));
+        System.out.println(keySet);
+        keySet.forEach(key ->
+                countList.stream()
+                        .map(line1 -> new ImmutablePair<String, Integer>(line1.getKey(), 1))
+                        .filter(line -> line.getKey().equals(key)) // 这里必须用filter过滤出相同的key值
+                        .reduce((line1, line2) ->
+                                new ImmutablePair<String, Integer>(line1.getKey(), line1.getValue() + line2.getValue()))
+                        .ifPresent(
+                                System.out::println
+                        )
+        );
     }
 }
